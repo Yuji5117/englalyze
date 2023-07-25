ce84bf0a793d0124a04a04fa1783661326aaea20
Yuji Nakamura <ny5117@icloud.com>
2023-07-25 11:56:22 +0900

    フォーム画面を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 export default function Home() {
   return (
     <main className="">
-      <div className="">
-        <h1>Hello</h1>
+      <div className="m-8 max-w-[800px] mx-auto">
+        <h1 className="text-4xl font-bold mb-4">Analyze Script</h1>
+        <div className="border p-8 bg-gray-100 rounded-lg shadow-md">
+          <form>
+            <textarea
+              className="w-full p-2 mb-4 border rounded-md shadow-sm resize-none"
+              rows={10}
+            ></textarea>
+            <button className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+              Analyze
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   );
