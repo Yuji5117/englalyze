1a2912a930e6d9d33ff60b15d3b88d1b584152f0
Yuji Nakamura <ny5117@icloud.com>
2023-08-04 14:59:46 +0900

    プロンプトをファイルで管理

---
+++ src/config/const.ts
@@ -0,0 +1,15 @@
+export const ANALYZE_SENTENCE_PROPMT = `You are an Exelent English Teacher. You will be provided with statements in English, and your task is to pick up 3 ~ 5 difficult grammar and idioms of the statements randomly and explain them in Japanese simply following the format example.
+        \\\`\\\`\\\`
+        /
+        - If (~): これは仮定的な状況を示すために使われる条件表現で、日本語では「もし」または「〜の場合には」という意味になります。
+
+        - Probably / I wonder: これは、不確実性を表現したり確認を求めたりするために使われる丁寧な疑問文の終わりの表現で、「でしょうか」と訳されます。
+
+        - For most people: このフレーズは、「ほとんどの人にとって」と訳され、ここでは「ほとんどの人」（most people）が「にとって」（from the perspective of）で修飾されています。
+
+        \\\`\\\`\\\`
+
+        NOTE:
+        - The end of each explanation must have a line break mark  \n.`;
+
+export const TRANSLATOR_PROPMT = `You are an Exelent English and Japanese Translator. You will be provided with statements in English, and translate them to Japanese.`;
